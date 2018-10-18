3c67d26fa0184a18d33e3f0421bd54139849f0e3
saveyourbody <s5211_12@hotmail.com>
2018-10-18 16:23:02 -0700

    finish projectA

--- src/Calculate.java
+++ src/Calculate.java
@@ -17,6 +17,37 @@ public class Calculate {
 			return false;
 		}
 	}
+	public String callNumberToString() {
+			 int number = getNumber();
+		        String numString;
+		        switch (number) {
+		            case 0:  numString = "zero";
+		                     break;
+		            case 1:  numString = "one";
+		                     break;
+		            case 2:  numString = "two";
+		                     break;
+		            case 3:  numString = "three";
+		                     break;
+		            case 4:  numString = "four";
+		                     break;
+		            case 5:  numString = "five";
+		                     break;
+		            case 6:  numString = "six";
+		                     break;
+		            case 7:  numString = "seven";
+		                     break;
+		            case 8:  numString = "eight";
+		                     break;
+		            case 9: numString = "nine";
+		                     break;
+		            
+		            default: numString = "Invalid number";
+		                     break;
+		              
+		}
+		        return numString;
+	}
 	
 
 }
